e85756d76fb5cf52dd65776c64b3282c94f0e9c6
Daniel <139485889+123shawoa@users.noreply.github.com>
2025-01-20 21:01:11 -0500

    reformed html

--- generator.py
+++ generator.py
@@ -1,12 +1,20 @@
 import qrcode
 import qrcode.constants
 
-qr = qrcode.QRCode(version=1,
+from flask import Flask, flash, redirect, render_template, request, session
+app = Flask(__name__)
+@app.route("/qrcode", methods=["GET", "POST"])
+def qrcode():
+    if request.method == "POST":
+        """turns a string into a QR code"""
+        qr = qrcode.QRCode(version=1,
                     error_correction= qrcode.constants.ERROR_CORRECT_L,
                     box_size=20,
                     border=2)
-qr.add_data("https://github.com/123shawoa/QR-code-generator")
-qr.make(fit = True)
+        qr.add_data("https://github.com/123shawoa/QR-code-generator")
+        qr.make(fit = True)
+    else:
+        return render_template("code.html")
 
 img = qr.make_image(fill_color = "black", back_color = "White")
 img.save("advanced.png")

c403fa1f82921469865dcdeddcd3f3e555977855
Daniel <139485889+123shawoa@users.noreply.github.com>
2025-01-20 21:10:25 -0500

    reformed generator

--- generator.py
+++ generator.py
@@ -7,14 +7,20 @@ app = Flask(__name__)
 def qrcode():
     if request.method == "POST":
         """turns a string into a QR code"""
+        data = request.form.get("data")
+        if not data:
+            flash("Please enter some data to create a QR code")
+        color = request.form.get("color")
+        if not color:
+            color = "black"
         qr = qrcode.QRCode(version=1,
                     error_correction= qrcode.constants.ERROR_CORRECT_L,
                     box_size=20,
                     border=2)
-        qr.add_data("https://github.com/123shawoa/QR-code-generator")
+        qr.add_data(data)
         qr.make(fit = True)
+        img = qr.make_image(fill_color = color, back_color = "White")
+        img.save("advanced.png")
     else:
         return render_template("code.html")
 
-img = qr.make_image(fill_color = "black", back_color = "White")
-img.save("advanced.png")
